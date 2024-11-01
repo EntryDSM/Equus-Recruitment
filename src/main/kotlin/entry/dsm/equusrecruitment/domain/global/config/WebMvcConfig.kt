b82536b24cd04d099d4662875d98e71946883006
나병현 <nbhyun0329@gmail.com>
2024-11-01 16:58:08 +0900

    🔧 chore: (#4) assign origin to a constant

--- src/main/kotlin/entry/dsm/equusrecruitment/domain/global/config/WebMvcConfig.kt
+++ src/main/kotlin/entry/dsm/equusrecruitment/domain/global/config/WebMvcConfig.kt
@@ -1,14 +1,20 @@
 package entry.dsm.equusrecruitment.domain.global.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
+
+    @Value("\${frontend.url}")
+    private val url = ""
+
     override fun addCorsMappings(registry: CorsRegistry) {
+
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins(url)
             .allowedMethods("*")
             .allowedHeaders("POST")
     }
