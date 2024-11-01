242b50342b389d686b268f068f8f50b0baeab01a
나병현 <nbhyun0329@gmail.com>
2024-11-01 16:34:01 +0900

    ✨ feat: (#4) add regular expression for phone numbers

--- src/main/kotlin/entry/dsm/equusrecruitment/domain/application/presentation/dto/request/InternApplicationRequest.kt
+++ src/main/kotlin/entry/dsm/equusrecruitment/domain/application/presentation/dto/request/InternApplicationRequest.kt
@@ -4,6 +4,7 @@ import entry.dsm.equusrecruitment.domain.application.domain.enum.Experience
 import entry.dsm.equusrecruitment.domain.application.domain.enum.Major
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class InternApplicationRequest(
@@ -12,8 +13,11 @@ data class InternApplicationRequest(
     val name: String,
     @field:NotNull
     val schoolNumber: Int,
-    @field:NotBlank
-    @Size(min = 13, max = 13)
+    @field:NotNull(message = "전화번호는 필수 입력 항목입니다")
+    @field:Pattern(
+        regexp = "^010-\\d{4}-\\d{4}$",
+        message = "올바른 전화번호 형식이 아닙니다. ex): 010-1234-5678"
+    )
     val phoneNumber: String,
     @field:NotBlank
     @Size(min = 1, max = 1000)
